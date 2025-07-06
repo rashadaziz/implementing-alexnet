4bdcd8211776aaa75dfcf4d9bfa06a2744ac7413
Rashad Aziz <rashadaziz.p@gmail.com>
2025-07-06 16:50:24 +0700

    feat: implement PCA data augmentation

---
+++ alexnet/augmentation/pca.py
@@ -0,0 +1,36 @@
+import torch
+from tqdm import tqdm
+
+class ImageNetPCA:
+    def __init__(self, dataloader: torch.utils.data.DataLoader):
+        self.dataloader = dataloader
+
+        self._running_sum = torch.zeros(3)
+        self._pixel_count = 0
+        self._running_outer_product = torch.zeros((3, 3))
+
+        self.eigenvalues = torch.zeros(3)
+        self.eigenvectors = torch.zeros((3, 3))
+    
+    def fit(self) -> tuple[torch.Tensor, torch.Tensor]:
+        with tqdm(total=len(self.dataloader.dataset), desc="Fitting PCA") as pbar:
+            for batch in self.dataloader:
+                image: torch.Tensor = batch['pixel_values'] # (batch_size, 3, height, width)
+                
+                self._running_sum += image.sum(dim=(0, 2, 3))
+                self._pixel_count += image.numel() // 3 # Assuming 3 channels
+
+                flattened_channels = image.transpose(1, 0).reshape(3, -1)
+
+                outer_product = torch.mm(flattened_channels, flattened_channels.t())
+                self._running_outer_product += outer_product
+                
+                pbar.update(image.size(0))
+
+        mean_vector = self._running_sum / self._pixel_count
+        mean_outer_product = self._running_outer_product / self._pixel_count
+        covariance_matrix = mean_outer_product - torch.outer(mean_vector, mean_vector)
+
+        self.eigenvalues, self.eigenvectors = torch.linalg.eigh(covariance_matrix)
+
+        return self.eigenvalues, self.eigenvectors
--- data/dataset.py
+++ data/dataset.py
@@ -3,8 +3,12 @@ from datasets import load_dataset
 from torch.utils.data import DataLoader
 from torchvision.transforms import Compose, Resize, CenterCrop, RandomCrop, TenCrop, RandomHorizontalFlip, Lambda, ToTensor
 
+from PIL import ImageFile
+ImageFile.LOAD_TRUNCATED_IMAGES = True
+
 training_dataset = load_dataset("ILSVRC/imagenet-1k", split="train")
 validation_dataset = load_dataset("ILSVRC/imagenet-1k", split="validation")
+test_dataset = load_dataset("ILSVRC/imagenet-1k", split="test")
 
 def collate_fn(batch):
     pixel_values = torch.stack([item['pixel_values'] for item in batch])
@@ -19,7 +23,9 @@ def get_dataloaders(batch_size=32, num_workers=4):
                             num_workers=num_workers, collate_fn=collate_fn)
     val_loader = DataLoader(validation_dataset, batch_size=batch_size, shuffle=False, 
                           num_workers=num_workers, collate_fn=collate_fn)
-    return train_loader, val_loader
+    test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False, 
+                            num_workers=num_workers, collate_fn=collate_fn)
+    return train_loader, val_loader, test_loader
 
 def get_dataloaders_with_transforms(batch_size=32, num_workers=4):
     training_dataset.set_transform(train_transforms)
@@ -29,7 +35,28 @@ def get_dataloaders_with_transforms(batch_size=32, num_workers=4):
                             num_workers=num_workers, collate_fn=collate_fn)
     val_loader = DataLoader(validation_dataset, batch_size=batch_size, shuffle=False, 
                           num_workers=num_workers, collate_fn=collate_fn)
-    return train_loader, val_loader
+    test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False, 
+                            num_workers=num_workers, collate_fn=collate_fn)
+    return train_loader, val_loader, test_loader
+
+def get_dataloaders_pca(sample_size=0.1, batch_size=256, num_workers=4):
+    if sample_size > 1 or sample_size < 0:
+        raise ValueError("Sample size must be between 0 and 1.")
+
+    training_dataset.set_transform(pca_transform)
+    validation_dataset.set_transform(pca_transform)
+
+    pca_subsample = training_dataset.train_test_split(test_size=sample_size, stratify_by_column="label")
+    pca_subsample_val = validation_dataset.train_test_split(test_size=sample_size, stratify_by_column="label")
+    pca_subsample_test = test_dataset.train_test_split(test_size=sample_size, stratify_by_column="label")
+
+    train_loader = DataLoader(pca_subsample["test"], batch_size=batch_size, shuffle=False, 
+                            num_workers=num_workers, collate_fn=collate_fn)
+    val_loader = DataLoader(pca_subsample_val["test"], batch_size=batch_size, shuffle=False, 
+                          num_workers=num_workers, collate_fn=collate_fn)
+    test_loader = DataLoader(pca_subsample_test["test"], batch_size=batch_size, shuffle=False, 
+                            num_workers=num_workers, collate_fn=collate_fn)
+    return train_loader, val_loader, test_loader
 
 def train_transforms(examples):
     transform = Compose(
@@ -61,6 +88,17 @@ def basic_transform(examples):
         [
             Resize(256),
             CenterCrop(256),
+            ToTensor(),
+        ]
+    )
+    examples["pixel_values"] = [transform(image.convert("RGB")) for image in examples["image"]]
+    return examples
+
+def pca_transform(examples):
+    transform = Compose(
+        [
+            Resize(128),
+            CenterCrop(128),
             ToTensor(),
         ]
     )
